a4d4a0a7715baaa1bbdd6e7aace8b7c467fb85d8
vuong.do <vuong.do@alta.com.vn>
2022-04-15 17:37:30 +0700

    feat(app): add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 import locale from './locale';
 import { RootState } from './store/configureStore';
 import Example from './view/Example';
+import Homepage from './view/Homepage';
+import PageNotFound from './view/PageNotFound';
 
 const App: React.FC = () => {
   const language = useSelector((state: RootState) => state.setting.language);
   const memoLanguageData = React.useMemo(() => locale[language], [language]);
   return (
     <IntlProvider locale={language} messages={memoLanguageData}>
-      <Example />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/example" element={<Example />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="teams/:teamId" element={<Homepage />} />
+        <Route path="teams/new" element={<Example />} />
+      </Routes>
     </IntlProvider>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { store } from './store/configureStore';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
